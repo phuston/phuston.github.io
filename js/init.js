12402769848fcb46579b62d4c46bcff23ce03a47
Patrick Huston <patrickhuston1@gmail.com>
2015-02-14 16:24:06 -0500

    Gets rid of scroll through 'lightbox' library on projects section

--- js/init.js
+++ js/init.js
@@ -103,23 +103,6 @@
 
 				}
 
-		// Main Sections: Two.
-
-			// Lightbox gallery.
-				$('#two').poptrox({
-					caption: function($a) { return $a.next('h3').text(); },
-					overlayColor: '#2c2c2c',
-					overlayOpacity: 0.85,
-					popupCloserText: '',
-					popupLoaderText: '',
-					selector: '.work-item a',
-					usePopupCaption: true,
-					usePopupDefaultStyling: false,
-					usePopupEasyClose: false,
-					usePopupNav: true,
-					windowMargin: (skel.isActive('small') ? 0 : 50)
-				});
-
 	});
 
 })(jQuery);
